ee44b629260a5af5d330a8ccbb584f5016ef0fec
katsuhah <hideki.katsuhara@justsystems.com>
2019-09-24 11:37:47 +0900

    spec1:  単語情報を出力
    - コメントを追加

--- src/code/Spec1.java
+++ src/code/Spec1.java
@@ -2,7 +2,8 @@ package code;
 
 public class Spec1 {
 
-	// 単語情報を出力できること
+	// 仕様：単語情報を出力できること。
+	// プログラムが実行されたら、なんらかの形で「上手」と出力する。
 	public void output() {
 		// 現状は標準出力で出力する
 		System.out.println("上手");
