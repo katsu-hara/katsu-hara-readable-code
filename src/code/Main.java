cbe2dfb8c9b4251445641f55ba1b3b09270682c7
katsuhah <hideki.katsuhara@justsystems.com>
2019-09-24 11:31:07 +0900

    spec1:  単語情報を出力

---
+++ src/code/Main.java
@@ -0,0 +1,10 @@
+package code;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		new Spec1().output();
+	}
+
+}
---
+++ src/code/Spec1.java
@@ -0,0 +1,10 @@
+package code;
+
+public class Spec1 {
+
+	// 単語情報を出力できること
+	public void output() {
+		// 現状は標準出力で出力する
+		System.out.println("上手");
+	}
+}
